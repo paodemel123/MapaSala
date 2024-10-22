6d0efb66b2edaecbfd54d424b83a9bef30219856
paodemel123 <carambola0202>
2024-10-22 10:50:05 -0300

    ARRUMAR OS ERROS EM FRM USUARUIO E MEXER NO CADASTRAR USUARIO

---
+++ MapaSala/Classes/Usuarios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaSala.Classes
+{
+    public class Usuarios
+    {
+        private SqlConnection Conexao =
+            new SqlConnection(
+                "Server=LS05MPF;Database=AULA_DS;User Id=sa; Password=admsasql");
+        public int Login { get; set; }
+        public int Senha { get; set; }
+        public int Ativo { get; set; }
+
+        public void Inserir()
+        {
+            Conexao.Open();
+            string query = "Insert into Usuarios (Login, Senha, Ativo" +
+            " Values (@login, @senha, @ativo ";
+            SqlParameter parametro1 = new SqlParameter("@login", Login);
+            SqlParameter parametro1 = new SqlParameter("@senha", Senha);
+            SqlParameter parametro1 = new SqlParameter("@ativo", Ativo);
+
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.Parameters.Add(parametro3);
+            comando.ExecuteNonQuery(); ;
+            Conexao.Close();
+
+
+        }
+    }
+}
---
+++ MapaSala/Formularios/FrmCadastrarUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapaSala.Formularios
+{
+    public partial class FrmCadastrarUsuario : Form
+    {
+        public FrmCadastrarUsuario()
+        {
+            InitializeComponent();
+        }
+
+      
+      
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
