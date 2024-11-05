df912386f03bbe05598f7548a90fc28b881c63ba
paodemel123 <drianymia1@gmail.com>
2024-11-05 10:21:47 -0300

    oiii

--- MapaSala/Program.cs
+++ MapaSala/Program.cs
@@ -16,7 +16,8 @@ namespace MapaSala
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Principal());
+            //Application.Run(new Principal());
+            Application.Run(new frmLogin());
         }
     }
 }
